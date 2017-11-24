b1d2a200efbea3783d0d8814636ab3515a0bc37b
tyx <897043280@qq.com>
2017-11-24 20:30:08 +0800

    修改了一些页面

--- app/src/main/java/com/tyx/mypractice/ui/ShoppingCarFragment.java
+++ app/src/main/java/com/tyx/mypractice/ui/ShoppingCarFragment.java
@@ -1,6 +1,15 @@
 package com.tyx.mypractice.ui;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+
 import com.tyx.mypractice.R;
+
+import butterknife.BindView;
 
 /**
  * Created by tyx on 2017/11/22.
@@ -8,9 +17,31 @@ import com.tyx.mypractice.R;
 
 public class ShoppingCarFragment extends BaseFragment {
 
+    @BindView(R.id.elv_pro)
+    ExpandableListView elvPro;
+
     @Override
     public int getLayoutResId() {
-        return R.layout.title_view_layout;
+        return R.layout.fragment_shopping_car;
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
