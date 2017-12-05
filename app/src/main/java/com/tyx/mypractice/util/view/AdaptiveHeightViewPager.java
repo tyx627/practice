177fff4d3102d9c65f23b830e47dcb8e320a495c
tyx <897043280@qq.com>
2017-12-05 23:07:44 +0800

    增加 禁止手势左右滑动的viewpager和自适应高度的viewpager

---
+++ app/src/main/java/com/tyx/mypractice/util/view/AdaptiveHeightViewPager.java
@@ -0,0 +1,60 @@
+package com.tyx.mypractice.util.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.HashMap;
+
+/**
+ * 禁止手势左右滑动
+ * 高度自适应内部view的viewpager
+ * Created by tyx on 2017/12/5.
+ */
+
+public class AdaptiveHeightViewPager extends NoScrollViewPager {
+
+    private int currentIndex;   // 当前页面的index
+    private int height = 0;
+    private HashMap<Integer, View> mChildrenViews = new HashMap<>();
+
+    public AdaptiveHeightViewPager(Context context) {
+        super(context);
+    }
+
+    public AdaptiveHeightViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mChildrenViews.size() > currentIndex){
+            View child = mChildrenViews.get(currentIndex);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            height = child.getMeasuredHeight();
+        }
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    // 重置高度
+    public void resetHeight(int currentIndex){
+        this.currentIndex = currentIndex;
+        if (mChildrenViews.size() > currentIndex){
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) getLayoutParams();
+            if (params == null){
+                params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, height);
+            } else {
+                params.height = height;
+            }
+            setLayoutParams(params);
+        }
+    }
+
+    // 保存对应的view和position
+    public void setViewAndPosition(View view, int position){
+        mChildrenViews.put(position, view);
+    }
+}
---
+++ app/src/main/java/com/tyx/mypractice/util/view/NoScrollViewPager.java
@@ -0,0 +1,43 @@
+package com.tyx.mypractice.util.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * 禁止手势左右滑动的viewpager
+ * Created by tyx on 2017/12/5.
+ */
+
+public class NoScrollViewPager extends ViewPager {
+
+    private boolean noScroll = false;
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (noScroll){
+            return false;
+        } else {
+            return super.onTouchEvent(ev);
+        }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (noScroll) {
+            return true;
+        } else {
+            return super.onInterceptTouchEvent(ev);
+        }
+    }
+
+}
