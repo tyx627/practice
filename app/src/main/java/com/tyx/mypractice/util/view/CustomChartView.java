39242ccf0b06ed5edfd3de4b137969f6b4f66e4d
tyx <897043280@qq.com>
2018-08-15 17:43:19 +0800

    修改一丢丢代码

--- app/src/main/java/com/tyx/mypractice/util/view/CustomChartView.java
+++ app/src/main/java/com/tyx/mypractice/util/view/CustomChartView.java
@@ -3,6 +3,7 @@ package com.tyx.mypractice.util.view;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -24,16 +25,18 @@ public class CustomChartView extends View {
     private Paint linePaintN;   // 画线的大小有差异，这是稍微细一点的画笔
     private Paint linePaintT;   // 用于写文字刻度的笔
     private Paint linePaintL;   // 用于画胎心曲线的笔
+    private Paint greenAreaPaint;   // 用于画绿色区域的笔
 
     private int screenWidth;    // 屏幕宽度
     private int viewHeight;   // 控件高度
-    private int oneMinWidth;    // 1分钟的宽度
-    private int twentySecWidth; // 20秒的宽度
-    private int fetalAreaHeight;    // 胎心曲线区域，垂直方向的高度
-    private int fetalLatticeHeight;    // 胎心曲线区域，垂直方向一个格子的高度
-    private int tocoAreaHeight;     // 宫缩曲线区域，垂直方向的高度
-    private int tocoLatticeHeight;    // 宫缩曲线区域，垂直方向一个格子的高度
-    private int tocoStartY;     // 宫缩曲线区域垂直方向起始位置
+    private float oneMinWidth;    // 1分钟的宽度
+    private float twentySecWidth; // 20秒的宽度
+    private float fetalAreaHeight;    // 胎心曲线区域，垂直方向的高度
+    private float fetalLatticeHeight;    // 胎心曲线区域，垂直方向一个格子的高度
+    private float tocoAreaHeight;     // 宫缩曲线区域，垂直方向的高度
+    private float tocoLatticeHeight;    // 宫缩曲线区域，垂直方向一个格子的高度
+    private float tocoStartY;     // 宫缩曲线区域垂直方向起始位置
+    private float fetalYPointHeight;  // 胎心区域垂直方向一个点的高度
 
     public CustomChartView(Context context) {
         this(context, null);
@@ -79,22 +82,31 @@ public class CustomChartView extends View {
         linePaintL = new Paint(Paint.ANTI_ALIAS_FLAG);
         linePaintL.setColor(mContext.getResources().getColor(R.color.color_333));
         linePaintL.setStrokeWidth(CommonUtil.dp2px(mContext, 1));
+
+        // 画绿色区域的画笔
+        greenAreaPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        greenAreaPaint.setColor(mContext.getResources().getColor(R.color.color_336DE016));
+        greenAreaPaint.setStyle(Paint.Style.FILL);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-        if (MeasureSpec.AT_MOST == heightMode){ // 高度是wrap
-            viewHeight = screenWidth;
+
+        if (MeasureSpec.EXACTLY == heightMode){
+            int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+            viewHeight = heightSize;
         } else {
-            viewHeight = MeasureSpec.makeMeasureSpec(heightMeasureSpec, MeasureSpec.EXACTLY);
+            viewHeight = (int)(screenWidth * 0.8);
         }
-        fetalAreaHeight = (int)(viewHeight * 0.64); // 胎心曲线区域总共占高度的0.64
+        fetalAreaHeight = (float) (viewHeight * 0.64); // 胎心曲线区域总共占高度的0.64
         fetalLatticeHeight = fetalAreaHeight / fetalLatticeNum; // 胎心曲线区域垂直方向每个格子的高度
-        tocoAreaHeight = (int)(viewHeight * 0.28); // 宫缩曲线区域总共占高度的0.28
+        tocoAreaHeight = (float) (viewHeight * 0.28); // 宫缩曲线区域总共占高度的0.28
         tocoLatticeHeight = tocoAreaHeight / tocoLatticeNum;    // 宫缩曲线区域垂直方向每个格子高度
-        tocoStartY = (fetalLatticeNum + 2) * fetalLatticeHeight;    // 宫缩区域和胎心区域，中间间隔高度为2个fetalLatticeHeight
+        tocoStartY = (fetalLatticeNum + 2) * fetalLatticeHeight;    // 宫缩区域和胎心区域，中间间隔高度为2个fetalLatticeHeight，胎心区域开始的高度
+        // 胎心区域垂直方向值为60-210，所以求出每个点所占高度
+        fetalYPointHeight = fetalAreaHeight / 160;
         setMeasuredDimension(screenWidth, viewHeight);
     }
 
@@ -102,6 +114,16 @@ public class CustomChartView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         drawBaseChart(canvas);
+        drawGreenArea(canvas);
+    }
+
+    // 画绿色背景区域
+    private void drawGreenArea(Canvas canvas) {
+        canvas.save();
+        // 绿色区域是110-160
+        Rect rect = new Rect(0, (int)(fetalYPointHeight * 50), screenWidth, (int)(fetalYPointHeight * 100));
+        canvas.drawRect(rect, greenAreaPaint);
+        canvas.restore();
     }
 
     // 画背景图
@@ -115,14 +137,6 @@ public class CustomChartView extends View {
                 canvas.drawLine(0, i * fetalLatticeHeight, screenWidth, i * fetalLatticeHeight, linePaintW);
             }
         }
-        // 胎心垂直区域背景
-        for (int i = 0; i <= 12; i++) {
-            if (i % 3 != 1){
-                canvas.drawLine(i * twentySecWidth, 0, i * twentySecWidth, fetalAreaHeight, linePaintN);
-            } else {
-                canvas.drawLine(i * twentySecWidth, 0, i * twentySecWidth, fetalAreaHeight, linePaintW);
-            }
-        }
         // 画宫缩横向区域背景
         for (int i = 0; i <= tocoLatticeNum; i++) {
             if (i % 2 == 0) {
@@ -131,9 +145,15 @@ public class CustomChartView extends View {
                 canvas.drawLine(0, i * tocoLatticeHeight + tocoStartY, screenWidth, i * tocoLatticeHeight + tocoStartY, linePaintN);
             }
         }
-        // 宫缩垂直区域背景
-        for (int i = 0; i < 12; i++) {
-
+        // 垂直区域背景
+        for (int i = 0; i <= 12; i++) {
+            if (i % 3 != 1){
+                canvas.drawLine(i * twentySecWidth, 0, i * twentySecWidth, fetalAreaHeight, linePaintN);
+                canvas.drawLine(i * twentySecWidth, tocoStartY, i * twentySecWidth, tocoStartY + tocoAreaHeight, linePaintN);
+            } else {
+                canvas.drawLine(i * twentySecWidth, 0, i * twentySecWidth, fetalAreaHeight, linePaintW);
+                canvas.drawLine(i * twentySecWidth, tocoStartY, i * twentySecWidth, tocoStartY + tocoAreaHeight, linePaintW);
+            }
         }
         canvas.restore();
     }
